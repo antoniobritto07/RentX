2f4348e32fb27a8f5d67016398e9e15338b4ee66
Antônio Britto <antonio.britto.2000@hotmail.com>
2022-02-08 21:36:09 -0300

    feat: new implementations using multer and other tools

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,55 @@
+import fs from "fs"; //modulo nativo do node => fileSystem
+import { parse } from "csv-parse";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IImportCategory {
+    name: string;
+    description: string;
+}
+
+class ImportCategoryUseCase {
+    constructor(private categoryRepository: ICategoriesRepository) { }
+
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const categories: Array<IImportCategory> = []
+            const stream = fs.createReadStream(file.path) //função que faz a leitura do arquivo em partes, melhorando a performance
+
+            const parseFile = parse();
+
+            stream.pipe(parseFile) //pipe é uma função que a cada pedaço lido, esse pipe vai mandando as infos aos poucos
+
+            parseFile.on("data", async (line) => {
+                const [name, description] = line;
+                categories.push({
+                    name,
+                    description
+                })
+            })
+                .on("end", () => {
+                    resolve(categories)
+                })
+                .on("error", (err) => {
+                    reject(err)
+                })
+        })
+    }
+
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categories = await this.loadCategories(file);
+
+        categories.map((category) => {
+            const { name, description } = category;
+
+            const existCategory = this.categoryRepository.findByName(name);
+            if (!existCategory) {
+                this.categoryRepository.create({
+                    name,
+                    description
+                })
+            }
+        })
+    }
+}
+
+export { ImportCategoryUseCase }
